0abd95b5a13ea01060fb42b4c3f5f39b519444b9
Gaurav <graj75657@gmail.com>
2024-12-24 15:09:01 +0530

    Create problem.java

---
+++ 03variable/problem.java
@@ -0,0 +1,15 @@
+//package 03variable;
+
+public class problem {
+    public static void main(String[] args) {
+      int a = 20;
+      int b = 10;
+      int ans;
+
+      //ans=(a*b)/(a-b)
+
+      ans=(a*b)/(a-b);
+      System.out.println("The answer is " + ans);
+    }
+    
+}
