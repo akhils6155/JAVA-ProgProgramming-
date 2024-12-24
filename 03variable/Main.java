33467e4567f96e9a3c7c3ca909b3159604b5ccb3
Gaurav <graj75657@gmail.com>
2024-12-24 15:08:56 +0530

    Create Main.java

---
+++ 03variable/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int age=20;
+        String name="AKhil Rajput";
+        System.out.println("Value of a is "+a);
+        System.out.println("Value of b is "+b);
+        System.out.println("Sum of a+b="+(a+b));
+        System.out.printf("Name is %s and age is %d",name,age);
+
+    }
+    
+}
