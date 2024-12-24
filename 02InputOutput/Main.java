d7b7ab4fd3315495fcb49d122716e88944b23dd1
Gaurav <graj75657@gmail.com>
2024-12-24 14:26:13 +0530

    Create Main.java

---
+++ 02InputOutput/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        //Output
+        System.out.print("This is First line");  //System.out.print() function display output in same line
+        System.out.print("This is Second line");
+
+        System.out.println("This is Third line"); //System.out.println() function display the output in the functin always new line
+        System.out.println("This is Fourth line");
+    }
+}
