fa59478f92c9edd6a27254f46c61f7f2bfae4c7e
Gaurav <graj75657@gmail.com>
2024-12-25 10:17:46 +0530

    Create main.java

---
+++ 05iteration/main.java
@@ -0,0 +1,26 @@
+// package 05iteration;
+
+public class main {
+    public static void main(String[] args) {
+        //for loop
+        System.out.println("For loop");
+        for(int i=1; i<=10; i++){
+            System.out.println(i);
+        }
+
+        System.out.println("While loop");
+        int j = 1;
+        while(j<=10){
+            System.out.println(j);
+            j++;
+        }
+
+        System.out.println("Do while loop");
+        int k = 1;
+        do{
+            System.out.println(k);
+            k++;
+        }while(k<=10);
+
+    }
+}
