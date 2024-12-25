f8d87aeff82a7b3b80821a4fe45a204884a25652
Gaurav <graj75657@gmail.com>
2024-12-25 10:18:08 +0530

    Create problem4.java

---
+++ 05iteration/problem4.java
@@ -0,0 +1,28 @@
+/*
+        *****
+        *   *
+        *   *
+        *****
+ */
+// package 05iteration;
+import java.util.Scanner;
+public class problem4 {
+    public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Enter the term : ");
+      int n = sc.nextInt();
+      for(int i=1; i<=n; i++){
+        for(int j=1; j<=n+1; j++){
+            if(i==1 || j==1 || i==n || j==n+1){
+                System.out.print("*");
+            }
+            else{
+                System.out.print(" ");
+            }
+        }
+        System.out.println();
+      }
+    
+      sc.close();  
+    }
+}
