8fe25f51bb53cc7355ee56f5ff59b68c9b4f0c2e
Gaurav <graj75657@gmail.com>
2024-12-25 10:30:52 +0530

    Create problem7.java

---
+++ 05iteration/problem7.java
@@ -0,0 +1,30 @@
+/*
+                Enter the number :
+                4
+                   *
+                  **
+                 ***
+                ****
+ */
+// package 05iteration;
+
+import java.util.Scanner;
+
+public class problem7 {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.println("Enter the number :");
+        int n = sc.nextInt();
+        for (int i=0; i<n; i++){
+            for(int j=0; j<n-i-1; j++){
+                System.out.print(" ");
+            }
+            for(int j=0; j<=i; j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+        sc.close();
+    }
+    
+}
