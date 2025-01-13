80f9b18d9c2dd9f9df41f9b1441ff5c862dce39a
Gaurav <graj75657@gmail.com>
2025-01-13 12:39:33 +0530

    Create problem12.java

---
+++ 05iteration/problem12.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class problem12 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int row=sc.nextInt();
+        for (int i=0; i<row; i++){
+            for (int j=0; j<row-i-1; j++){
+                System.out.print(" ");
+            }
+            for (int j=0; j<=i;j++){
+                System.out.print(i+1+" ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
