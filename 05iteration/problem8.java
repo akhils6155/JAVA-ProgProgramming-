d824368171c4943462d543411bb5efb68df45a1a
Gaurav <graj75657@gmail.com>
2025-01-13 12:39:13 +0530

    Create problem8.java

---
+++ 05iteration/problem8.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class problem8 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int row =sc.nextInt();
+        int num=1;
+        for (int i=0; i<row; i++){
+            for (int j=0; j<=i; j++ ){
+                System.out.print(num+" ");
+                num++;
+            }
+            System.out.println("");
+        }
+        sc.close();
+    }
+}
